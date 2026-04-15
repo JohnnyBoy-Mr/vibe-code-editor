172c297063a9725c6dea45078e0cd201c452b3f4
JohnnyBoy-Mr <JohnhenryLinks@gmail.com>
2026-04-15 13:29:15 +0200

    08-playground-file-explorer-1

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+    REACT: "/vibecode-starters/react-ts",
+    NEXTJS: "/vibecode-starters/next-new",
+    EXPRESS: "/vibecode-starters/express-simple",
+    VUE: "/vibecode-starters/vue",
+    ANGULAR: "/vibecode-starters/angular",
+    HONO: "/vibecode-starters/hono-nodejs-starter",
+}
